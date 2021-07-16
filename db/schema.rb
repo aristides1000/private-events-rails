c7594e5ca17088a3ea5939ed482c0080aa8e856d
aristides1000 <aristides1000@gmail.com>
2021-07-16 17:13:25 -0400

    modify date to datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_165229) do
+ActiveRecord::Schema.define(version: 2021_07_16_210733) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "event_id"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_07_16_165229) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
-    t.time "date"
+    t.datetime "date"
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
